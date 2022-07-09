0de9d58a4f32c0b206d290a742167e959b04d42d
Marcus Watkins <me@marcuswatkins.net>
2022-07-08 19:54:14 -0600

    Remove wx from gui.h

--- gui/gui.h
+++ gui/gui.h
@@ -3,16 +3,8 @@
 #ifndef GUI_WX_H
 #define GUI_WX_H
 
-#include <wx/statusbr.h>
-#include <wx/frame.h>
-#include <wx/numdlg.h>
-#include <wx/dnd.h>
-#include <wx/mstream.h>
-#include <wx/filename.h>
-#include <wx/config.h>
 #include <sys/time.h>
 #if (wxUSE_UNICODE)
-#include <wx/encconv.h> 
 #endif
 
 #include "guilcd.h"
@@ -23,46 +15,24 @@ enum
 	ID_LCD,
 };
 
-class WabbitemuFrame: public wxFrame
+class WabbitemuFrame
 {
 public:
     WabbitemuFrame(LPCALC);
-   	wxWindow *wxLCD;
     
 
-	void OnKeyDown(wxKeyEvent& event);
-	void OnKeyUp(wxKeyEvent& event);
 	void SetSpeed(int speed);
 	int gui_draw();
 	void gui_frame_update();
-	void OnTimer(wxTimerEvent& event);
-protected:
-	DECLARE_EVENT_TABLE()
 private:
 	bool is_resizing;
 
-	void OnPauseEmulation(wxCommandEvent& event);
-	void OnTurnCalcOn(wxCommandEvent& event);
-	
-	void OnSetSpeed(wxCommandEvent& event);
-	void OnSetSpeedCustom(wxCommandEvent& event);
-	void OnSetSize(wxCommandEvent& event);
-		
-	void OnPaint(wxPaintEvent& event);
-	// Resize
-	void OnResize(wxSizeEvent& event);
-	void OnShow(wxShowEvent& event);
 	
 	// We use "isShownVar" instead of "isShown" because "isShown" is
 	// an existing wxWidgets method
 	int isShownVar = 0;
 	
 	LPCALC lpCalc;
-	
-	void OnLeftButtonDown(wxMouseEvent& event);
-	void OnLeftButtonUp(wxMouseEvent& event);
-	void OnSize(wxSizeEvent& event);
-	void OnQuit(wxCloseEvent& event);
 };
 int SetGIFName();
 WabbitemuFrame* gui_frame(LPCALC lpCalc);
